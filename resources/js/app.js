efe24800ab93e5a60c534516bcaadf87d554732b
Josh Whitwell <josh.whitwell@gmail.com>
2024-01-13 21:23:20 -0500

    Refactor title function in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 const appName = import.meta.env.VITE_APP_NAME || "Josh Whitwell";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: () => appName,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
