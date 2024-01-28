bf4790b0f53759bfc2ff2375910543e86dfaad72
Josh Whitwell <josh.whitwell@gmail.com>
2024-01-28 17:04:59 -0500

    Fix login route name

--- routes/auth.php
+++ routes/auth.php
@@ -10,7 +10,8 @@ Route::middleware('guest')->group(function () {
 
     Route::get('enter', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])
+        ->name('login');
 
     // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
     //     ->name('password.request');
