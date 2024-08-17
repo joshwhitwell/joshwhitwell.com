b6442c9c4579460118278a871249b54eee38508b
Josh Whitwell <josh.whitwell@gmail.com>
2024-08-17 17:03:53 -0400

    add private tag to non-public writings

--- resources/views/my/writings/index.blade.php
+++ resources/views/my/writings/index.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout title="Writings">
-
   <x-slot name="header">
     <h1>Writings</h1>
   </x-slot>
@@ -7,19 +6,23 @@
   <a href="{{ route('my.writings.create') }}" class="button">+ New Writing</a>
 
   @if ($writings->isNotEmpty())
-    <ol>
-      @foreach ($writings as $writing)
-        <li>
-          <h2>
-            <a href="{{ route('my.writings.edit', $writing) }}">{{ $writing->internalTitle }}</a>
-          </h2>
-          <p>{{ $writing->created_at->format('M j, Y') }}</p>
-          {!! $writing->body !!}
-        </li>
-      @endforeach
-    </ol>
+  <ol>
+    @foreach ($writings as $writing)
+    <li>
+      <h2 class="flex align-items-center gap-1">
+        <a href="{{ route('my.writings.edit', $writing) }}"
+          >{{ $writing->internalTitle }}
+        </a>
+        @if (!$writing->visibility)
+        <div class="body pill-tag pill-tag--red">Private</div>
+        @endif
+      </h2>
+      <p>{{ $writing->created_at->format('M j, Y') }}</p>
+      {!! $writing->body !!}
+    </li>
+    @endforeach
+  </ol>
   @endif
 
   {{ $writings->links() }}
-
 </x-app-layout>
