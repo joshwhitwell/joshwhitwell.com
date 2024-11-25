04cb68f2f07afcdd80105ffca1766e0122878eb5
Josh Whitwell <josh.whitwell@gmail.com>
2024-11-25 15:18:36 -0500

    feat: add recipe scaffolding

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipe;
+use App\Http\Requests\RecipeRequest;
+
+class RecipeController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(RecipeRequest $request)
+    {
+        //
+    }
+
+    public function show(Recipe $recipe)
+    {
+        //
+    }
+
+    public function edit(Recipe $recipe)
+    {
+        //
+    }
+
+    public function update(RecipeRequest $request, Recipe $recipe)
+    {
+        //
+    }
+
+    public function destroy(Recipe $recipe)
+    {
+        //
+    }
+}
