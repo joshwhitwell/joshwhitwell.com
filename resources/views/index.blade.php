9a1d37467fd00170aa7a6677e1a1f2f3d631021e
Josh Whitwell <josh.whitwell@gmail.com>
2025-03-09 12:42:12 -0400

    add app-navigation-drawer styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,14 +7,49 @@
 
   <title>{{ config('app.name') }}</title>
 
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=menu" />
+
   @if (file_exists(public_path('build/manifest.json'))
-    || file_exists(public_path('hot'))
-	)
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-	@endif
+  || file_exists(public_path('hot'))
+  )
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @endif
 </head>
 
-<body></body>
+<body>
+  <div id="app" class="app">
+    {{-- app-bar --}}
+    <header class="app-bar">
+      <div class="app-bar__row">
+        <section class="app-bar__section">
+          <button class="app-icon-button" onclick="window._toggleAppNavigationDrawer()">
+            <span class="material-symbols-outlined">
+              menu
+            </span>
+          </button>
+        </section>
+      </div>
+    </header>
+
+    {{-- navigation-drawer --}}
+    <div id="app-navigation-drawer" class="app-navigation-drawer">
+      <nav class="app-navigation-drawer__nav">
+        <button class="app-icon-button" onclick="window._toggleAppNavigationDrawer()">
+          <span class="material-symbols-outlined">
+            menu
+          </span>
+        </button>
+
+        <ol>
+          @for ($i = 0; $i < 100; $i++) <li>
+            The current value is {{ $i }}
+            </li>
+            @endfor
+        </ol>
+      </nav>
+    </div>
+  </div>
+</body>
 
 </html>
-
