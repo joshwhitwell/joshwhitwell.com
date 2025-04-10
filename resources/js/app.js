06882a9683190cf0a9b73dce17243d1c2e87d3ae
Josh Whitwell <josh.whitwell@gmail.com>
2025-04-10 19:12:52 -0400

    feat: remove styles and js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,4 @@
 import axios from "axios";
+
 window.axios = axios;
-
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
-const appNavigationDrawer = document.getElementById('app-navigation-drawer');
-
-function toggleAppNavigationDrawer() {
-  if (appNavigationDrawer) {
-    if (appNavigationDrawer.hasAttribute('data-open')) {
-      appNavigationDrawer.removeAttribute('data-open')
-    } else {
-      appNavigationDrawer.setAttribute('data-open', '')
-    }
-  }
-}
-
-window._toggleAppNavigationDrawer = toggleAppNavigationDrawer
