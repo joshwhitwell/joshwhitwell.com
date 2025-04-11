62b3255c70b290e02af4bac3f30c7aa5982343c5
Josh <josh.whitwell@journeygroup.com>
2025-04-11 16:37:51 -0400

    feat: install inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
 
 window.axios = axios;
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+createInertiaApp({
+  resolve: (name) => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+    return pages[`./Pages/${name}.vue`];
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el);
+  },
+});
