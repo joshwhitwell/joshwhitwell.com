da5074eff9265873803a47c9a73c17a7d742d3af
Josh Whitwell <josh.whitwell@gmail.com>
2025-10-04 11:33:56 -0400

    install laravel

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use function env;
+use function now;
+use function bcrypt;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $shouldSeedAdminUser = env('ADMIN_NAME')
+            && env('ADMIN_EMAIL')
+            && env('ADMIN_PASSWORD')
+            && !User::where('email', env('ADMIN_EMAIL'))->exists();
+
+        if ($shouldSeedAdminUser) {
+            User::create([
+                'name' => env('ADMIN_NAME'),
+                'email' => env('ADMIN_EMAIL'),
+                'email_verified_at' => now(),
+                'password' => bcrypt(env('ADMIN_PASSWORD')),
+            ]);
+        }
+    }
+}
