6f1dd96a7255b503ca84819484c845f9dcac0ced
Josh Whitwell <josh.whitwell@gmail.com>
2025-10-26 20:20:15 -0400

    fix justify-self-end on safari

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,7 +1,7 @@
 <div>
   <button
-    class="focus:outline-offset-3 flex min-h-[40px] items-center gap-x-2 justify-self-end rounded-sm bg-neutral-700 pl-2 pr-3 text-white focus:outline-neutral-600"
-    type="{{ $type ?? "submit" }}"
+    class="focus:outline-offset-3 ml-auto flex min-h-[40px] items-center gap-x-2 rounded-sm bg-neutral-700 pl-2 pr-3 text-white focus:outline-neutral-600"
+    type="{{ $type ?? 'submit' }}"
   >
     @if ($slot->hasActualContent())
       {{ $slot }}
